55eb2626cb269874ce1dce752962e36688546800
ggg50 <xjfletitgo@gmail.com>
2019-07-12 17:37:22 +0800

    基本骨架完成

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      newTodo: "test",
+      todoList: [
+        {id: 1, title: "First events"}
+      ]
+    }
+  }
+  render(){
+    let todos = this.state.todoList.map(todo => {
+      return <li>{todo.title}</li>;
+    })
+    return (
+      <div>
+        <h1>我的代办</h1>
+        <div><input /></div>
+        <ul>{todos}</ul>
+      </div>
+    )
+  }
 }
+
+ReactDOM.render(<App />, document.querySelector("#root"));
 
 export default App;
