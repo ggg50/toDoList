cb64745a418db5e168a8b1283bbfa40f95c4f8b6
ggg50 <xjfletitgo@gmail.com>
2019-07-13 23:06:28 +0800

    userName and password inputable

--- app/src/userDialog.js
+++ app/src/userDialog.js
@@ -6,13 +6,28 @@ export default class UserDialog extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      selected: 'signUp'
+      selected: 'signUp',
+      formData: {
+        userName:"",
+        password:""
+      }
     }
-  }
+  };
   switch(e) {
     this.setState({
       selected: e.target.value
     })
+  };
+
+  changeUserName(e){
+    let newState = JSON.parse(JSON.stringify(this.state));
+    newState.formData.userName = e.target.value;
+    this.setState(newState)
+  }
+  changePassword(e){
+    let newState = JSON.parse(JSON.stringify(this.state));
+    newState.formData.password = e.target.value;
+    this.setState(newState)
   }
 
   render(){
@@ -21,11 +36,11 @@ export default class UserDialog extends React.Component{
       <form className="signUp">
         <div className="row">
           <label>用户名</label>
-          <input type="text" />
+          <input type="text" value={this.state.formData.userName} onChange={this.changeUserName.bind(this)} />
         </div>
         <div className="row">
           <label>密码</label>
-          <input type="password" />
+          <input type="password" value={this.state.formData.password} onChange={this.changePassword.bind(this)} />
         </div>
         <div className="row actions">
           <button type="submit">注册</button>
@@ -36,11 +51,11 @@ export default class UserDialog extends React.Component{
         <form className="signIn">
           <div className="row">
             <label>用户名</label>
-            <input type="text" />
+            <input type="text" value={this.state.formData.userName} onChange={this.changeUserName.bind(this)} />
           </div>
           <div className="row">
             <label>密码</label>
-            <input type="password" />
+            <input type="password" value={this.state.formData.password} onChange={this.changePassword.bind(this)} />
           </div>
           <div className="row actions">
             <button type="submit">登录</button>
